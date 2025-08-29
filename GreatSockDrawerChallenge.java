c6475b73b4d4ade57722dda1363c1a40bdb8fcb3
Waris Sripatoomrak <167295008+waris-sri@users.noreply.github.com>
2025-08-29 14:31:49 +0700

    refactor: tidy up code

--- GreatSockDrawerChallenge.java
+++ GreatSockDrawerChallenge.java
@@ -1,33 +1,32 @@
 public class GreatSockDrawerChallenge {
-    public static void main(String[] args) {
-        Sock socks = new Sock();
-        socks.addNewSock("Red(Clean)");
-        socks.addNewSock("Blue (Hole)");
-        socks.addNewSock("Striped (Clean)");
-        socks.addNewSock("Green (Lost)");
-        socks.addNewSock("Polka Dot (Clean)");
-        socks.addNewSock("Black (Hole)");
+  public static void main(String[] args) {
+    Sock socks = new Sock();
+    socks.addNewSock("Red(Clean)");
+    socks.addNewSock("Blue (Hole)");
+    socks.addNewSock("Striped (Clean)");
+    socks.addNewSock("Green (Lost)");
+    socks.addNewSock("Polka Dot (Clean)");
+    socks.addNewSock("Black (Hole)");
 
-        // Person 1
-        socks.markDamaged(4);
-        socks.listAllSocks();
+    // Person 1
+    socks.markDamaged(4);
+    socks.listAllSocks();
 
-        // Person 2
-        socks.findMatchingPair("Red");
-        socks.addNewSock("Yellow (Clean)");
+    // Person 2
+    socks.findMatchingPair("Red");
+    socks.addNewSock("Yellow (Clean)");
 
-        // Person 3
-        socks.repairSocks(4);
-        socks.removeSockByIndex(5);
+    // Person 3
+    socks.repairSocks(4);
+    socks.removeSockByIndex(5);
 
-        // Person 4
-        socks.findLostSocks();
-        socks.discardSock("Hole");
+    // Person 4
+    socks.findLostSocks();
+    socks.discardSock("Hole");
 
-        // Person 5
-        socks.countTotalSocks();
-        socks.countDamagedSocks();
-        socks.clearDrawer();
-    }
+    // Person 5
+    socks.countTotalSocks();
+    socks.countDamagedSocks();
+    socks.clearDrawer();
+  }
 }
-
